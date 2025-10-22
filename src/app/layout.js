a50135d471b8b7bde5b4cda9c1dd582168dbfee3
Oleg Polyakov <olegpolyakov@outlook.com>
2025-10-22 19:54:12 +0300

    chore: remove commented-out script tag from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,6 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&family=Shantell+Sans:ital,wght@1,300..400&display=swap" />
         <link rel="stylesheet" href="https://s3.regru.cloud/sayyes-static/styles/shared.css" />
         <link rel="stylesheet" href="https://sayyes.school/wp-content/themes/sayyes/static/styles/main.css" />
-        {/* <script src="https://s3.regru.cloud/sayyes-static/scripts/shared.js" async></script> */}
       </head>
 
       <body>
