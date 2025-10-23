4b7e452fe29a89b6a69fa03fff0c2024badc0bb5
Oleg Polyakov <olegpolyakov@outlook.com>
2025-10-23 15:44:57 +0300

    Add new favicon images for the application
    - Added favicon-192x192.png with a size of 192x192 pixels.
    - Added favicon-32x32.png with a size of 32x32 pixels.

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <head>
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon-192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/favicon-180x180.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&family=Shantell+Sans:ital,wght@1,300..400&display=swap" />
         <link rel="stylesheet" href="https://s3.regru.cloud/sayyes-static/styles/shared.css" />
